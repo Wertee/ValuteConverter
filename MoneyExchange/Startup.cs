7d76201f2b021a107de32520d63130aadc0eeea2
Александр Павлов <Waterbin1993@gmail.com>
2023-02-23 12:54:21 +0300

    Refactoring

--- MoneyExchange/Startup.cs
+++ MoneyExchange/Startup.cs
@@ -25,7 +25,7 @@ namespace ValuteConverter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddScoped(typeof(Converter), typeof(Converter));
+            services.AddSingleton(typeof(Converter), typeof(Converter));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
